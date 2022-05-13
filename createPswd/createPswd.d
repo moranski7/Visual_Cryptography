cc56205271d59582e27b207a327d70b008212a77
Michael <Michael.Oranski@gmail.com>
2022-05-13 11:05:18 -0700

    Revert "testing"
    This reverts commit 08b5728c3cfc05d0fdb24b2393e29d92059c43b1.

---
+++ createPswd/createPswd.d
@@ -0,0 +1,119 @@
+/*
+	Reference: 
+	C++ program to generate random password 
+	https://www.includehelp.com/cpp-programs/generate-random-password.aspx
+	Hritik Raj
+	June 21, 2018 
+*/
+import std.stdio;
+import std.string;
+import std.conv;
+import std.random;
+import core.stdc.stdlib;
+
+/*
+	checks the arguements passed to the program.
+
+	Param:
+		args  	- args passed to program
+		length 	- Pass by reference. Contains length of the password generated.
+	Return:
+		N/A
+*/
+void checkArg (string[] args, ref int length);
+
+/*
+	Generates a password of specified length.
+
+	Param:
+		length 	- Determines the length of the password.
+
+	Return:
+		A password containing 3 of the 4 class characteristics.
+*/
+string genPswd (int length);
+
+void main (string[] args) {
+
+	int length = 0;
+	checkArg (args, length);	// Validates the command line arguments and gets length.
+	string pswd = genPswd (length); // Generate a password of 
+	
+	//Store in file.
+	File fl = File (args[1], "w");
+	fl.writeln (pswd);
+	fl.close();
+
+	exit (0);
+}
+
+void checkArg (string[] args, ref int length) {
+	
+
+	// If only one command line argument provided, default to password length of 7.
+	if (args.length == 2)
+		length = 7;
+	// If argument provided, check to see if it is a number and within range.
+	else if (args.length == 3) {
+		if (isNumeric(args[2])){
+			length = to!int(args[2]); // converts argument to an integer.
+			if (length < 6) {
+				stderr.writeln ("Length must be greater or equal to 6 characters long.");
+				exit (1);
+			}
+			else if (length > 50) {
+				stderr.writeln ("Length can't exceed 50 characters in length.");
+				exit(1);
+			}
+		}
+		else {
+			stderr.writeln ("Argument must be number.");
+			exit (1);
+		}
+	}
+	//User provided too many command line arguments.
+	else {
+		stderr.writeln ("Usage: ./createPswd fileName [optional] #");
+		exit (1);
+	}
+}
+
+/*
+	Generate a random number based on a specified range.
+*/
+auto getKey (int range) {
+	auto rand = Random(unpredictableSeed);		// Get random generator with a new seed after each run.
+	auto key = uniform (0, range, rand);  // generates a uniformly-distributed integer in the range of [1, 100]
+	return (key % range);
+}
+
+string genPswd (int length) {
+	int count = 0;
+	string pswd = "";
+	// Three of four possible password characteristic class: Upper, Lower, number
+	string lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
+	string upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string numeric = "0123456789";
+
+	while (count < length) {
+		auto pswdCharClass = getKey (3);	// Choose a password characteristic class at random
+
+		switch (pswdCharClass) {
+
+			case 0	:	int select = getKey (26); // select from lower class
+						pswd = pswd ~ lowerAlpha[select.. (select+1)]; //slice from lower string and add to password.
+						break;
+			case 1	:	int select = getKey (26); // select from upper
+						pswd = pswd ~ upperAlpha[select.. (select+1)]; //slice from lower string and add to password.
+						break;
+			case 2	:	int select = getKey (10); // select from number
+						pswd = pswd ~ numeric[select.. (select+1)]; //slice from lower string and add to password.
+						break;
+			default :
+		}
+
+		count++;
+	}
+
+	return pswd;
+}
---
+++ emailImage/sendImage.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+#Reference:
+# https://stackoverflow.com/questions/37201250/sending-email-via-gmail-python
+# Author: apadana
+# Date: May 17, 2016
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import oauth2client
+from oauth2client import client, tools, file
+from apiclient import errors, discovery
+import mysql.connector
+import httplib2
+import base64
+import sys
+import os
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~') #>> C:\Users\Me
+    credential_dir = os.path.join(home_dir, '.credentials')  # >>C:\Users\Me\.credentials   (it's a folder)
+    
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir) #create folder if doesnt exist
+    credential_path = os.path.join(credential_dir, 'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path) #Store the credential
+    credentials = store.get()
+    
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)  # Create a flow object. (it assists with OAuth 2.0 steps to get user authorization + credentials)
+        flow.user_agent = APPLICATION_NAME
+
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    
+    return credentials
+
+def createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      msgHtml: Html message to be sent
+      msgPlain: Alternative plain text message for older email clients          
+      attachmentFile: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+
+    #create the message container using a dictionary { to, from, subject }
+    message = MIMEMultipart('mixed')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+
+    messageA = MIMEMultipart('alternative')
+    messageR = MIMEMultipart('related')
+
+    #Add text to email body.
+    messageR.attach(MIMEText(msgHtml, 'html'))
+    messageA.attach(MIMEText(msgPlain, 'plain'))
+    messageA.attach(messageR)
+
+    message.attach(messageA)
+
+    # Try to guess what kind of file is being attached.
+    content_type, encoding = mimetypes.guess_type(attachmentFile)
+    main_type, sub_type = content_type.split('/', 1)
+ 
+    #how the file should be read and stored. Should Only be dealing with image.
+    if main_type == 'image':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    else: #If it reaches here, then there is something wrong with file checking part.
+        print ("ERROR: FILE NOT AN IMAGE!")
+        exit (1)
+
+    #Encode the attachment, add a header and attach it to the message
+    filename = os.path.basename(attachmentFile)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    #encode the message (the message should be in bytes)
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+def SendMessageInternal(service, user_id, message):
+    """Sends the email while checking for any error"""
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def SendMessage(sender, to, subject, msgHtml, msgPlain, attachFile):
+	"""Get the credentials, create the email and send it. """
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http()) # Create an httplib2.Http object to handle our HTTP requests, and authorize it using credentials.authorize()
+	service = discovery.build('gmail', 'v1', http=http)
+	message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachFile)
+	result = SendMessageInternal(service, "me", message1) #Sends email and checks for errors
+
+	return result
+
+def getEmail (userName):
+    mydb = mysql.connector.connect (
+        host="localhost",
+        user="Michael Oranski",
+        password="Password@2",
+        database='VoteDatabase',
+        auth_plugin='mysql_native_password'
+    )
+    cursor = mydb.cursor()
+
+    sql = "SELECT Email FROM voters WHERE UserName = %s"
+    val = (userName, )
+    cursor. execute (sql, val)
+
+    result = cursor. fetchall ()
+    voterEmail = result[0][0]
+
+    return voterEmail
+
+def main ():
+	if (len(sys.argv) < 3):
+		print ("Not enough arguments!")
+		print ("Usage: ./sendImage.py imageFile userName")
+		exit (1)
+
+	elif (len (sys.argv) > 3):
+		print ("Too many arguments!")
+		print ("Usage: ./sendImage.py imageFile userName")
+		exit (1)
+
+	#Make sure first argument is a picture file.
+	if (not (sys.argv[1].lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')))):
+		print ("First command line argument must be a png, jpg, jpeg, or bmp file.")
+		exit (1)
+
+	#Email contents
+	to = getEmail (sys.argv[2])
+	sender = "Michael.Oranski@gmail.com"
+	subject = "Sending Share Test"
+	msgHtml = "Hi<br/>Here is the share for the pswd."
+	msgPlain = "Hi\nHere is the share for the pswd."
+	attachFile = sys.argv[1]
+
+	SendMessage(sender, to, subject, msgHtml, msgPlain, attachFile)
+
+
+if __name__ == '__main__':
+    main()
+    exit (0)
